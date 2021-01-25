0d19a3552ef5cc5baacf222a70e5c0c67f44efcc
Dmitriy Kuleshoff <77134524+Kuleshoff-researcher@users.noreply.github.com>
2021-01-25 12:54:40 +0700

    Create CART_Classification.py
    Classification and regression trees (CART or just decision trees) create a binary tree from training data. Split points are eagerly selected by evaluating each attribute and each value of each attribute in the training data to minimize the cost function.

---
+++ CART_Classification.py
@@ -0,0 +1,18 @@
+# CART Classification
+import pandas
+from sklearn import model_selection
+from sklearn.tree import DecisionTreeClassifier
+url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/pima-indians-diabetes.data.csv"
+names = ['preg', 'plas', 'pres', 'skin', 'test', 'mass', 'pedi', 'age', 'class']
+dataframe = pandas.read_csv(url, names=names)
+array = dataframe.values
+X = array[:,0:8]
+Y = array[:,8]
+seed = 7
+kfold = model_selection.KFold(n_splits=10, random_state=seed)
+model = DecisionTreeClassifier()
+results = model_selection.cross_val_score(model, X, Y, cv=kfold)
+print(results.mean())
+
+# Выполнение примера выводит среднюю оценочную точность.
+# 0.692600820232
